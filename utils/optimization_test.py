c10e45db3cf8fa1f1415679eff4282e04eb66894
John Link <jwl9vq@virginia.edu>
2023-10-18 11:28:11 -0400

    added new stuff

--- utils/optimization_test.py
+++ utils/optimization_test.py
@@ -27,7 +27,7 @@ VARS = (
     data['rear_brake_pressure'].to_numpy()
 )
 observed_accel = data['expected_accel'].to_numpy()
-observed_accel_filtered = gaussian_filter1d(observed_accel, 20)
+observed_accel_filtered = gaussian_filter1d(observed_accel, 10)
 
 torque_matrix = np.loadtxt(os.path.join(current_path, 'dyno_matrix_maker/new_data/output_matrix.csv'), delimiter=',')
 # torque_matrix_np = torque_matrix.to_numpy()
@@ -44,14 +44,15 @@ def acceleration_on_car(VARS, c1, c2, a1, a2, a3, b1):
     et = (c1 * torque_matrix[filtered_rpm, thr.astype(int)] * final_drive * gear_ratios[gear.astype(int)]) / 0.3113
 
     engine_decel = (c2 * final_drive * gear_ratios[gear.astype(int)]) / 0.3113
-    engine_decel[et > 200] = 0
+    engine_decel[np.logical_or(et > 250, thr < 3)] = 0
+    engine_decel[rpm < 1500] = 0
 
     et[rpm < 1600] = 0.0
     et[thr < 3] = 0.0
     
     brake_decel = (b1 * gaussian_filter1d(bkpf + bkpr, 10)) / 0.3113
     brake_decel[v < 0.5] = 0
-    rolling = (a2 * v) + a3
+    rolling = np.ones_like(filtered_rpm) * a3
     rolling[v < 0.5] = 0
     
     return (et - brake_decel - (v**2 * a1) - engine_decel)/750 - rolling
@@ -61,12 +62,13 @@ def objective(params):
     # Your code to compute the objective
     return np.sum((observed_accel_filtered - acceleration_on_car(VARS, *params)) ** 2)
 
-kernel = C(1.0, (1e-3, 1e4)) * RBF(10, (1e-2, 1e3))
-gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=100)
+kernel = C(1.0, (1e-3, 1e5)) * RBF(10, (1e-2, 1e3))
+gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
 
 
 # Assume 6 parameters in your model
-initial_points = np.random.rand(1000, 6)  # 10 initial random points, change dimensions accordingly
+tries = 200
+initial_points = np.column_stack([np.random.rand(tries) * .4 + .55, np.random.rand(tries) * 50 + 40, np.random.rand(tries) * 0.3 + 0.1, np.random.rand(tries), np.random.rand(tries), np.random.rand(tries) * 0.2 + .3])  # 10 initial random points, change dimensions accordingly
 initial_objective = np.array([objective(params) for params in initial_points])
 
 gp.fit(initial_points, initial_objective)
@@ -79,7 +81,7 @@ def acquisition_function(params):
 
 
 for i in range(50):  # Number of optimization iterations
-    next_point = minimize(acquisition_function, [0.55, 0.38, 0.02, 0.007, 0.59, 0.5], bounds=[(0.5, 0.9), (0, 1), (0, 1), (0, 1), (0, 1), (0, 1)], options={'maxiter': 10000}).x
+    next_point = minimize(acquisition_function, [0.65, 40, 0.4, 0.00, 0.59, 0.3], options={'maxiter': 10000}).x
     next_objective = objective(next_point)
     gp.fit(np.vstack([initial_points, next_point]), np.hstack([initial_objective, next_objective]))
 
@@ -92,6 +94,8 @@ print(optimal_params)
 
 predicted_accel_redefined = acceleration_on_car(VARS, *optimal_params)
 
+original_predicted_accel_redefined = acceleration_on_car(VARS, *[0.85, 40, 0.4, 0.00, 0.59, 0.4])
+
 # Calculate residuals with the new parameters
 residuals_redefined = observed_accel_filtered - predicted_accel_redefined
 
@@ -101,6 +105,7 @@ fig, axs = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
 # Plot real vs predicted acceleration
 axs[0].plot(observed_accel_filtered, label='Filtered Real Acceleration', color='blue', linewidth= 0.5)
 axs[0].plot(predicted_accel_redefined, label='Predicted Acceleration', color='red', linewidth= 0.5)
+axs[0].plot(original_predicted_accel_redefined, label='Predicted Acceleration', color='orange', linewidth= 0.5)
 axs[0].set_title('Filtered Real vs Predicted Acceleration (Redefined Function)')
 axs[0].set_xlabel('Index')
 axs[0].set_ylabel('Acceleration')
